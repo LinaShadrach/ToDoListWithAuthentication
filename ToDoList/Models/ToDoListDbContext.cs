bc373b27112fb58489a5096de984b7da34a1341c
Lina Shadrach <lina.shadrach@gmail.com>
2017-11-27 09:24:11 -0800

    add AppUser class

--- ToDoList/Models/ToDoListDbContext.cs
+++ ToDoList/Models/ToDoListDbContext.cs
@@ -1,3 +1,26 @@
-﻿<? xml version="1.0" encoding="utf-8" ?>
-<configuration>
-</configuration>
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace ToDoList.Models
+{
+    public class ToDoListDbContext : IdentityDbContext<AppUser>
+    {
+        //public DbSet<Item> Items { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    => optionsBuilder
+        //        .UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options) : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
+
