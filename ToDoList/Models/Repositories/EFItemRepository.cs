b1dc32ad72bbddf18e315eec26084776ac71d804
Lina Shadrach <lina.shadrach@gmail.com>
2017-11-28 16:14:57 -0800

    add Item class

---
+++ ToDoList/Models/Repositories/EFItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ToDoList.Models.Repositories
+{
+    public class EFItemRepository : IItemRepository
+    {
+        ToDoListDbContext db = new ToDoListDbContext();
+
+        public IQueryable<Item> Items { get; }
+
+        public Item Edit(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item Save(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoList/Models/Repositories/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ToDoList.Models.Repositories
+{
+    public interface IItemRepository
+    {
+        IQueryable<Item> Items { get; }
+        Item Save(Item item);
+        Item Edit(Item item);
+        void Remove(Item item);
+    }
+}
